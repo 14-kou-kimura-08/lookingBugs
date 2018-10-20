e9742f01361706f2d44dd9c6d91114682aa2d952
Kou <kimura@kurashicom.jp>
2018-10-21 00:39:56 +0900

    Create store method

--- app/Http/Controllers/MeetingsController.php
+++ app/Http/Controllers/MeetingsController.php
@@ -3,8 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Meeting;
+use App\user;
 
 class MeetingsController extends Controller
 {
-    //
+    public function __construct(Meeting $meeting, User $user)
+    {
+        $this->meeting = $meeting;
+        $this->user    = $user;
+    }
+
+    public function store(Request $request)
+    {
+        $this->meeting->save();
+
+        $this->user->name = $request->name;
+        $this->user->meeting_id = $this->meeting->id;
+        $this->user->admin = 1;
+        $this->user->save();
+        /// NOTE: redirect先を変更する
+        return redirect('/');
+    }
 }
