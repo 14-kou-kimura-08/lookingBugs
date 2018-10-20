903385af682cc580e0c0bfee7af3bd5f28949648
Kou <kimura@kurashicom.jp>
2018-10-20 22:01:37 +0900

    Create migration files

---
+++ database/migrations/2018_10_20_121559_create_meetings_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMeetingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('meetings', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('member_num')->default(4);
+            $table->integer('time')->default(3);
+            $table->timestamp('started_at')->nullable();
+            $table->timestamp('ended_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('meetings');
+    }
+}
